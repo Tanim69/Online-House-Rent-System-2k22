5ec7a9980db940fb753d79b6674050893fdc1455
Tanim69 <tanimkhan117@gmail.com>
2022-04-07 21:40:48 +0600

    new update

---
+++ app/Http/Controllers/Admin/TenantController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tenant;
+
+
+class TenantController extends Controller
+{
+
+    public function list(){
+        $tenants=Tenant::all();
+        return view('backend.layouts.tenant', compact('tenants'));
+
+    }
+    public function create(Request $request)
+    {
+        //dd(request->all);
+        //insert into tenant table
+        Tenant::create([
+            'id'=>$request->id,
+            'name'=>$request->name,
+            'address'=>$request->address,
+            'occupation'=>$request->occupation,
+            'email'=>$request->email,
+            'phonenumber'=>$request->phonenumber,
+        ]);
+        return redirect()->back();
+    }
+
+        public function delete($id)
+{
+Tenant::find($id)->delete();
+return redirect()->back();
+}
+
+}
---
+++ resources/views/backend/partials/sidebar.blade.php
@@ -0,0 +1,87 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+
+
+
+
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('dashboard')}}">
+              <span data-feather="home"></span>
+              <button type="Submit" class="btn btn-primary">Dashboard</button>
+
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('house') }}">
+              <span data-feather="home"></span>
+              <button type="Submit" class="btn btn-primary">Houseowner</button>
+
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('houseinfo.list')}}">
+              <span data-feather="home"></span>
+              <button type="Submit" class="btn btn-primary">Houseinfo</button>
+
+            </a>
+          </li>
+
+
+
+
+
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('tenant')}}">
+              <span data-feather="home"></span>
+               <button type="Submit" class="btn btn-primary">Tenant</button>
+
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('show.booking')}}">
+              <span data-feather="home"></span>
+              <button type="Submit" class="btn btn-primary">Booking</button>
+
+
+            </a>
+          </li>
+
+
+
+
+
+
+
+
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('payment.list')}}">
+              <span data-feather="home"></span>
+              <button type="Submit" class="btn btn-primary">Payment</button>
+
+
+
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('report')}}">
+              <span data-feather="home"></span>
+              <button type="Submit" class="btn btn-primary">Report</button>
+
+
+
+            </a>
+          </li>
+
+
+
+
+
+          </ul>
+
+
+          </div>
+          </nav>
